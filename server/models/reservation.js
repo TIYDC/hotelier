e84ed3e3e96f0dd4a13af267571439740f5a29a9
Jordan Kasper <jordan@jordankasper.com>
2016-12-02 15:11:12 -0500

    Added auto-addition of staff ID to new reservation

--- server/models/reservation.js
+++ server/models/reservation.js
@@ -1,5 +1,13 @@
 'use strict';
 
-module.exports = function(Reservation) {
+var loopbackContext = require('loopback-context');
 
+module.exports = function(Reservation) {
+    Reservation.observe('before save', function addAuthor(ctx, next) {
+        if (ctx.isNewInstance) {
+            var token = loopbackContext.getCurrentContext().get('accessToken');
+            ctx.instance.staffId = ((token) ? token.userId : null);
+        }
+        next();
+    });
 };
