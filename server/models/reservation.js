b964454a4d0caeaf2e9b5a539d790df4c95b673d
Jordan Kasper <jordan@jordankasper.com>
2016-12-02 16:21:26 -0500

    Added double booking check in reservations

--- server/models/reservation.js
+++ server/models/reservation.js
@@ -10,4 +10,35 @@ module.exports = function(Reservation) {
         }
         next();
     });
+
+    Reservation.validateAsync('roomId', checkForDoubleBooking, {
+        message: 'This room is already booked for that date'
+    });
+
+    function checkForDoubleBooking(err, done) {
+        var timestamp = (this.checkinDate instanceof Date) ?
+            this.checkinDate.getTime() :
+            this.checkinDate;
+        var resDate = new Date(timestamp);
+        if (!resDate.getTime()) {
+            return err();
+        }
+
+        resDate.setHours(0);
+        resDate.setMinutes(0);
+        var startTs = resDate.getTime();
+        var endTs = startTs + (86400000 * this.numberOfNights);
+
+        Reservation.count({
+            roomId: this.roomId,
+            and: [
+                { checkinDate: { gte: startTs } },
+                { checkinDate: { lte: endTs } }
+            ]
+        }, function(err, conflicts) {
+            if (err || conflicts) {
+                return err();
+            }
+        });
+    }
 };
