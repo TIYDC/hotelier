b8fc4d73c1d98980c5f9abf5399dff57aba8cfb8
Jordan Kasper <jordan@jordankasper.com>
2016-12-03 08:56:41 -0500

    Updated production mongo connect details

--- server/datasources.production.js
+++ server/datasources.production.js
@@ -2,10 +2,6 @@ module.exports = {
   'db': {
     'name': 'db',
     'connector': 'mongodb',
-    'host': process.env.DB_HOST || null,
-    'port': process.env.DB_PORT || 27017,
-    'database': process.env.DB_NAME || null,
-    'username': process.env.DB_USER || 'admin',
-    'password': process.env.DB_PASS || '*****'
+    'url': process.env.MONGODB_URI || null
   }
 };
